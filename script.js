2eb5ce3a93628c0d4af74934419515c6319af889
Pamidimarri vijay sankar <sankerpamidimarri@gmail.com>
2024-11-21 12:22:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function stringChop(str, size) {
   // your code here
-	if(size==0){
+	if(size==0 || str.length==0){
 		return [];
 	}
 	
@@ -15,6 +15,6 @@ function stringChop(str, size) {
 }
 
 // Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
+// const str = prompt("Enter String.");
+// const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
